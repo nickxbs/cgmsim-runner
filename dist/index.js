9d77c8d39adbacc3b831b0f93c3f4df6b0dbdf0c
Nicola Febbrari <febbrari@gmail.com>
2022-06-03 22:52:33 +0200

    up

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,43 @@ const cron = require("node-cron");
 dotenv.config();
 const env = process.env;
 const logLevel = env.LOG_LEVEL;
+let lastState;
+function runUVA() {
+    return tslib_1.__awaiter(this, void 0, void 0, function* () {
+        try {
+            const NS = {
+                APISECRET: env.APISECRET,
+                NIGHTSCOUT_URL: env.NIGHTSCOUT_URL,
+            };
+            const params = {
+                ISF: env.ISF,
+                CR: env.CR,
+                DIA: env.DIA,
+                WEIGHT: env.EIGHT,
+                TP: env.TP,
+                CARBS_ABS_TIME: env.CARBS_ABS_TIME,
+            };
+            console.log(`NS:`, NS);
+            console.log(`params:`, params);
+            const down = yield cgmsim_lib_1.downloads(NS.NIGHTSCOUT_URL, NS.APISECRET);
+            console.log(`test0, download`);
+            const treatments = down.treatments;
+            const entries = down.entries;
+            const profiles = down.profiles;
+            const newEntry = cgmsim_lib_1.simulatorUVA({ treatments, env: params, lastState, profiles, entries });
+            lastState = newEntry.x;
+            console.log(`test1, main`);
+            yield cgmsim_lib_1.uploadEntries({
+                sgv: Math.round(newEntry.y.Gp),
+                direction: 'Flat',
+            }, NS.NIGHTSCOUT_URL, NS.APISECRET);
+            console.log(`test2, upload to `, env.NIGHTSCOUT_URL);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    });
+}
 function run() {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         try {
@@ -36,7 +73,9 @@ function run() {
             }, NS.NIGHTSCOUT_URL, NS.APISECRET);
             console.log(`test2, upload to `, env.nsUrl);
             if (logLevel === 'debug') {
-                const notes = `min:${newEntry.deltaMinutes}<br>
+                const notes = `
+			sgv:${newEntry.sgv}<br>
+			min:${newEntry.deltaMinutes}<br>
 			carb:${newEntry.carbsActivity.toFixed(4)}<br>
 			bas:${newEntry.basalActivity.toFixed(4)}<br>
 			bol:${newEntry.bolusActivity.toFixed(4)}<br>
@@ -51,6 +90,8 @@ function run() {
         }
     });
 }
+// runUVA();
+// const cronJob = cron.schedule(`*/5 * * * *`, runUVA, false); //periodically execution. First after 5 minutes. 
 run();
 const cronJob = cron.schedule(`*/5 * * * *`, run, false); //periodically execution. First after 5 minutes. 
 cronJob.start();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
 import * as dotenv from 'dotenv';
-import { simulator, downloads, uploadEntries, uploadNotes } from '@lsandini/cgmsim-lib';
+import { simulator, downloads, uploadEntries, simulatorUVA, uploadNotes } from '@lsandini/cgmsim-lib';
 import * as cron from 'node-cron';
 
 dotenv.config();
 const env = process.env;
 const logLevel = env.LOG_LEVEL;
 
+let lastState
 
 
+async function runUVA() {
+	try {
+
+
+		const NS = {
+			APISECRET: env.APISECRET,
+			NIGHTSCOUT_URL: env.NIGHTSCOUT_URL,
+		};
+		const params = {
+			ISF: env.ISF,
+			CR: env.CR,
+			DIA: env.DIA,
+			WEIGHT: env.EIGHT,
+			TP: env.TP,
+			CARBS_ABS_TIME: env.CARBS_ABS_TIME,
+		};
+
+		console.log(`NS:`, NS);
+		console.log(`params:`, params);
+
+		const down = await downloads(NS.NIGHTSCOUT_URL, NS.APISECRET);
+		console.log(`test0, download`);
+		const treatments = down.treatments;
+		const entries = down.entries;
+		const profiles = down.profiles;
+		const newEntry = simulatorUVA({ treatments, env: params, lastState, profiles, entries });
+		lastState = newEntry.x;
+		console.log(`test1, main`);
+		await uploadEntries({
+			sgv: Math.round(newEntry.y.Gp),
+			direction: 'Flat',
+		}, NS.NIGHTSCOUT_URL, NS.APISECRET);
+		console.log(`test2, upload to `, env.NIGHTSCOUT_URL);
+	} catch (e) {
+		console.log(e);
+	}
+
+}
 async function run() {
 	try {
 
@@ -40,7 +79,9 @@ async function run() {
 		}, NS.NIGHTSCOUT_URL, NS.APISECRET);
 		console.log(`test2, upload to `, env.nsUrl);
 		if (logLevel === 'debug') {
-			const notes = `min:${newEntry.deltaMinutes}<br>
+			const notes = `
+			sgv:${newEntry.sgv}<br>
+			min:${newEntry.deltaMinutes}<br>
 			carb:${newEntry.carbsActivity.toFixed(4)}<br>
 			bas:${newEntry.basalActivity.toFixed(4)}<br>
 			bol:${newEntry.bolusActivity.toFixed(4)}<br>
@@ -54,6 +95,8 @@ async function run() {
 	}
 
 }
+// runUVA();
+// const cronJob = cron.schedule(`*/5 * * * *`, runUVA, false); //periodically execution. First after 5 minutes. 
 run();
 const cronJob = cron.schedule(`*/5 * * * *`, run, false); //periodically execution. First after 5 minutes. 
 cronJob.start();
